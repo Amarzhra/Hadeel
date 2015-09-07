a1e5bcd5be29f7ff5e44e71f32de3291b2b19695
Amar Zhra <ayz_1980@yahoo.com>
2015-09-07 12:58:26 +0200

    added new lab

---
+++ Lab 1 new.R
@@ -0,0 +1,133 @@
+
+
+ my_num_vector<-function(x){
+ a<-c(log(11,base=10),cos(pi/5),exp(pi/3),(1173%%7)/19)
+ return(a)
+ }
+ my_num_vector(x)
+#[1] 1.0413927 0.8090170 2.8496539 0.2105263
+
+name<-"Amar Zhra"
+liuid<-"amazh110"
+
+install.packages("Rcpp")
+devtools::install_github("MansMeg/markmyassignment")
+
+library(markmyassignment)
+lab_path <-
+  "https://raw.githubusercontent.com/MansMeg/AdvRCourse/master/Labs/Tests/lab1.yml"
+set_assignment(lab_path)
+
+markmyassignment::mark_my_assignment(task="my_num_vector")
+
+
+filter_my_vector<-function(x,leq){
+  x[x>=leq]<-NA
+  return(x)
+  
+}
+name<-"Amar Zhra"
+liuid<-"amazh110"
+
+
+calculate_elements<-function(A){
+  A<-matrix(nrow = a,ncol = b)
+  z<-(a*b)
+  return(z)
+  
+  
+  
+}
+name<-"Amar Zhra"
+liuid<-"amazh110"
+
+
+
+row_to_zero<-function(A,i){
+  A<-matrix[i,j]
+  apply(1:nrow, 1,x*0)
+  return(A)
+  
+  
+}
+name<-"Amar Zhra"
+liuid<-"amazh110"
+
+
+add_elements_to_matrix<-function(A,x,i,j){
+  A<-matrix[i,j]
+  apply(A,x+m[i,j])
+  return(A)
+  
+  
+  
+  
+}
+name<-"Amar Zhra"
+liuid<-"amazh110"
+
+
+
+dot_prod<-function(a,b){
+  for(i in 1:N){
+    x<-sum(a[i]*b[i])
+    return(x)
+    
+    
+  }
+  dot_prod(a<-(1,1,1),b<-(2,2,2)
+  
+  name<-"Amar Zhra"
+  liuid<-"amazh110"
+  
+  
+  my_num_vector<-function(x){
+    a<-c(log(11,base=10),cos(pi/5),exp(pi/3),(1173%%7)/19)
+    return(a)
+  }
+  my_num_vector(x)
+  my_magic_matrix<-matrix(c(4,9,2,3,5,7,8,1,6),nrow = 3,byrow = T)
+  my_magic_matrix
+  
+  
+  my_magic_list<-list(info="my own list",my_num_vector,my_magic_matrix)
+  my_magic_list
+  name<-"Amar Zhra"
+  liuid<-"amazh110"
+  
+  
+  
+  my_magic_matrix<-matrix(c(4,9,2,3,5,7,8,1,6),nrow = 3,byrow = T)
+  my_magic_matrix
+  
+  name<-"Amar Zhra"
+  liuid<-"amazh110"
+  
+  
+  My_data.frame<-data.frame(list(id=c(1,2,3),name=c("John","Liza","Azra"),income=c(7.30,0.00,15.20),rich=c("FALSE","FALSE","TRUE")))
+  My_data.frame
+  
+  
+  name<-"Amar Zhra"
+  liuid<-"amazh110"
+  
+  
+  
+  approx_e<-function(N){
+    n<-0:N
+    e<-sum(1/!n)
+    return(e)
+      
+  }
+   
+   markmyassignment::mark_my_assignment(task="approx_e(N)")
+   
+   a_list<-my_magic_list
+   change_info<-function(x,text){
+     x<-list
+     apply(x,info=text)
+     
+     
+   }
+change_info(x=a_list,text = "some new info")
+  
